f911f35e478bcd9cb306db6752efac74809c1eaa
Andy Tu <andytu@alumni.ubc.ca>
2015-02-24 22:02:55 -0800

    Optimization- revert to attribute, do not select elements with formatted='true'

--- fbformat.js
+++ fbformat.js
@@ -1,8 +1,11 @@
 var chatCount = 0;
 
+//$('#ChatTabsPagelet').ready(function() {
+
+//});
 $(window).load( function() {
 
-  setInterval(function(){checkUpdate()},2000);
+  setInterval(function(){checkUpdate()},5000);
 
   /*var injectedScript = document.createElement('script');
   injectedScript.src = chrome.extension.getURL('inject.js');
@@ -18,14 +21,13 @@ $(window).load( function() {
 // Returns a jQuery object of all fb chat window messages in view.
 function fetchMsgs(chat_lists) {
     //console.log(chat_lists);
-    chat_lists = chat_lists.not('.emoticon,.emoticon_text');
 
     chat_lists = chat_lists.filter( function() {
-      //console.log(jQuery.data(this,'formatted'));
-      //console.log(this.innerHTML);
-      if(this.innerHTML.match(/(<span)|(<a)/)
-      //|| this.getAttribute('formatted') == 'true'){
-      || jQuery.data(this,'formatted') == true){
+
+      //console.log($(this).attr('formatted'));
+      if(this.innerHTML.match(/(<span)|(<a)/)) {
+      //|| $(this).attr('formatted') == 'true'){
+        $(this).attr('formatted','true');
         return false;
       }
       return true;
@@ -39,20 +41,22 @@ function process(messages) {
 
   messages.each( function(index, msg) {
     //msg.innerHTML = msg.innerText;
+    //console.log($(msg).attr('formatted'));
     var m = $(msg);
-    m.html(m.text());
-    //msg.setAttribute('formatted',true);
-    m.data('formatted', true);
+    m.html(msg.innerText);
+    m.attr('formatted','true');
+    //m.data('formatted', true);
   });
 }
 
 // Check if new messages have appeared.
 function checkUpdate() {
   var bench = performance.now();
-
-  var chats = $('#ChatTabsPagelet .fbNub div.conversation div.direction_ltr span span');
-  //var chats = convo.find('div.direction_ltr span span');
-//console.log(convos);
+  //var chats = $("#ChatTabsPagelet .fbNub div.conversation div.direction_ltr span span");
+//console.log(chats);
+console.log('chatlog');
+  var chats = $("#ChatTabsPagelet .fbNub div.conversation div.direction_ltr span span:not([formatted='true'], .emoticon, .emoticon_text)");
+console.log(chats);
   var prev = chatCount;
   chatCount = chats.length;
 
