759718e273d8dfc6d3a217c85a5c70331edbda38
Andy Tu <andytu@alumni.ubc.ca>
2015-02-24 20:48:38 -0800

    add benchmarking timer, further optimizations

--- fbformat.js
+++ fbformat.js
@@ -17,12 +17,15 @@ $(window).load( function() {
 
 // Returns a jQuery object of all fb chat window messages in view.
 function fetchMsgs(chat_lists) {
-
+    //console.log(chat_lists);
     chat_lists = chat_lists.not('.emoticon,.emoticon_text');
 
     chat_lists = chat_lists.filter( function() {
+      //console.log(jQuery.data(this,'formatted'));
       //console.log(this.innerHTML);
-      if(this.innerHTML.match(/(<span)|(<a)/)){
+      if(this.innerHTML.match(/(<span)|(<a)/)
+      //|| this.getAttribute('formatted') == 'true'){
+      || jQuery.data(this,'formatted') == true){
         return false;
       }
       return true;
@@ -33,27 +36,32 @@ function fetchMsgs(chat_lists) {
 }
 
 function process(messages) {
+
   messages.each( function(index, msg) {
-    if( msg.getAttribute('formatted') != 'true'){
-      msg.innerHTML = msg.innerText;
-      msg.setAttribute('formatted',true);
-    }
+    //msg.innerHTML = msg.innerText;
+    var m = $(msg);
+    m.html(m.text());
+    //msg.setAttribute('formatted',true);
+    m.data('formatted', true);
   });
 }
 
 // Check if new messages have appeared.
 function checkUpdate() {
-  var convo = $('#ChatTabsPagelet .fbNub div.conversation');
-  var chats = convo.find('div.direction_ltr span span');
+  var bench = performance.now();
 
+  var chats = $('#ChatTabsPagelet .fbNub div.conversation div.direction_ltr span span');
+  //var chats = convo.find('div.direction_ltr span span');
+//console.log(convos);
   var prev = chatCount;
   chatCount = chats.length;
 
   if (chatCount > prev) {
     fetchMsgs(chats);
-    console.log(chats);
+    //console.log(chats);
   } else {
     chats.splice(0,chatCount);
-    console.log(chats);
+    //console.log(chats);
   }
+  console.log(performance.now() - bench);
 }
