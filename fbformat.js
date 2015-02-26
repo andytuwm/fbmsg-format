41a666d48210d89f4a25533e004e5b414bfa93da
Andy Tu <andytu@alumni.ubc.ca>
2015-02-25 21:55:57 -0800

    fix issue#6 - emoticons break tags

--- fbformat.js
+++ fbformat.js
@@ -1,7 +1,7 @@
 var chatCount = 0;
 
 $(window).load( function() {
-
+  //checkUpdate();
   setInterval(function(){checkUpdate()},2000);
 
   /*var injectedScript = document.createElement('script');
@@ -18,16 +18,36 @@ function filterMsgs(chat_lists) {
     //console.log(chat_lists);
     chat_lists = chat_lists.not('.emoticon,.emoticon_text');
     chat_lists = chat_lists.filter( function() {
-      //console.log($(this).attr('formatted'));
-      if(this.innerHTML.match(/(<span)|(<a)/)) {
+
+      // If <span> starts with <span data and/or has a link, throw that
+      // element away since there is another <span> that will be processed.
+      if(this.innerHTML.match(/(^(<span data))|(<a)/)) {
         // Despite removing it from processing, we still need to mark it as formatted
         // to prevent the next poll from selecting this element.
         $(this).attr('formatted','true');
         return false;
       }
+
+      // If this element has two or more children, it must contain emoticons.
+      // Ignore <span>'s that have only one children; emoticons consist of two <span>'s.
+      // We will process this first before process() because it's easier.
+      var t = $(this);
+      if (t.children().length > 1) {
+        // Split the span element into emoticons and text nodes, then check them one by one
+        t.contents().each( function(index, el) {
+          if(el.nodeType === 3) {
+            // If el is a text node(3), wrap it in <span> and turn it into HTML.
+            var newEl = $(el).wrap("<span></span>").parent();
+            newEl.html(newEl.text());
+          }
+        });
+        // Mark as formatted.
+        t.attr('formatted','true');
+        return false;
+      }
+
       return true;
     });
-
   //console.log(chat_lists);
   process(chat_lists);
 }
